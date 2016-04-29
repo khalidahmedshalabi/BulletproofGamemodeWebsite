be1c037fc97600e68386dab95a9ba302e2d3bbcf
Khalid <khalidahmed333@hotmail.com>
2016-04-29 22:22:38 +0200

    Fixes #6 and adds comments to JS code

---
+++ resources/js/main.js
@@ -0,0 +1,76 @@
+// Click button to navigate to section
+$("nav").find("a").click(function(e) 
+{
+	// Prevent default event to make ours happen
+	e.preventDefault();
+	
+	// Get the value of 'href' attribute for the clicked element
+	var section = $(this).attr("href");
+	
+	// Scroll to it with animation
+	$("html, body").animate(
+	{
+		scrollTop: $(section).offset().top
+	});
+});
+
+// Make the user click the default nav button when the main page loads so that external sub pages load
+$("#DefaultNavButton").trigger("click");
+	
+// Load external sub pages
+function LoadContent(page)
+{
+	$(".Content").load(page);
+}
+
+/*	Nav bar positioning based on user scrolling	*/
+
+var IsNavigatorInFixedPos = false; // A variable to indicate whether the nav bar is placed in a fixed position or not
+var NavigatorFirstTopOffset = $(".Navigator").offset().top; // Initialize nav position
+
+// On scroll event...
+$(window).scroll(function()
+{
+	// Check if the user has scrolled enough. In other words, the nav bar is not visible anymore in viewpoint.
+	if($(window).scrollTop() > NavigatorFirstTopOffset)
+	{
+		// See if it's not in a fixed position already
+		if(!IsNavigatorInFixedPos)
+		{
+			// Set fixed position
+			$(".Navigator").css("position", "fixed");
+			$(".Navigator").css("top", "0%");
+			IsNavigatorInFixedPos = true;
+		}
+	}
+	else if(IsNavigatorInFixedPos)
+	{
+		// If the user is still near the top of the main page and nav bar is in a fixed position
+		
+		// Return it to its default position
+		$(".Navigator").css("position", "static");
+		IsNavigatorInFixedPos = false;
+	}
+})
+
+// Set window scroll value to the top of content page so that user can read everything clearly
+function ScrollToTopOfContent()
+{
+	$(window).scrollTop(NavigatorFirstTopOffset);
+}
+
+// Handle nav button clicks
+$(".NavButtons").click
+(
+	function()
+	{
+		// Remove the css 'clicked' class from all other elements
+		$(".NavButtons").removeClass("NavButtonsClicked");
+		
+		// Set class style to this element only
+		$(this).addClass("NavButtonsClicked");
+		
+		// Scroll to top of the content area
+		ScrollToTopOfContent();
+	}
+);
