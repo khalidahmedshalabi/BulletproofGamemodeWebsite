62cbdc1c4f172053eeeb2f917e1f41939173d802
Khalid <khalidahmed333@hotmail.com>
2016-05-01 04:06:59 +0200

    Download page - closes #3

---
+++ resources/js/download.js
@@ -0,0 +1,48 @@
+$(window).resize
+(
+	function()
+	{
+		$(".MainSections").animate
+		(
+			{
+				width: ($(window).width() * 0.5)
+			},
+			{
+				queue: false,
+				duration: 300
+			}
+		);
+	}
+);
+
+$(".MainSections").hover
+(
+	function()
+	{
+		$(".MainSections").animate
+		(
+			{
+				width: ($(window).width() * 0.3)
+			},
+			{
+				queue: false,
+				duration: 800
+			}
+		);
+		$(".MainSections").css("background-color", "#000000");
+		$(".MainSections").addClass("Blurry");
+		
+		$(this).animate
+		(
+			{
+				width: ($(window).width() * 0.7)
+			},
+			{
+				queue: false,
+				duration: 800
+			}
+		);
+		$(this).css("background-color", "#111111");
+		$(this).removeClass("Blurry");
+	}
+);
