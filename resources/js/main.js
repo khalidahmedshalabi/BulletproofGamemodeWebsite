916dc52369217adbbb82bf2ca83356abd7234aa9
Khalid <khalidahmed333@hotmail.com>
2016-06-03 15:20:55 +0200

    Adds blurry effect to background image

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,12 @@
 // Click button to navigate to section
-$("nav").find("a").click(function(e) 
+$("nav").find("a").click(function(e)
 {
 	// Prevent default event to make ours happen
 	e.preventDefault();
-	
+
 	// Get the value of 'href' attribute for the clicked element
 	var section = $(this).attr("href");
-	
+
 	// Scroll to it with animation
 	$("html, body").animate(
 	{
